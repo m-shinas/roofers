00318df0ef0abb3e026e408e6b46a3007303283f
Mohammed Shinas <mshinas555@gmail.com>
2025-09-16 18:43:08 +0530

    homepage update

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,56 @@
 "use client";
 
-import { useState } from "react";
+// import { useState } from "react";
 import Hero from "./ui/Hero";
-import Background from "./ui/Background";
+// import Background from "./ui/Background";
+import Image from "next/image";
+import { mulish } from "./ui/fonts";
+import Link from "next/link";
 
 export default function Home() {
-  const [heroCount, setHeroCount] = useState(0);
+  // const [heroCount, setHeroCount] = useState(0);
 
   return (
     <>
       <Hero />
       {/* <Background heroCount={heroCount}/> */}
-      <h1 className="h-[60vh] text-center ">Home</h1>
+      <section className="padding">
+        <div className="flex justify-between items-center max-xl:flex-col-reverse gap-10">
+          <div className=" max-w-[700px] max-h-[600px]">
+            <Image
+              src="/hero_sec.jpg"
+              alt="Roof work"
+              width={773}
+              height={687}
+              className="object-contain w-full rounded-2xl"
+            />
+          </div>
+          <div className="flex flex-col">
+            <h2
+              className={`text-tall-poppy-700 text-4xl capitalize lg:max-w-lg font-bold`}
+            >
+              Building Strength. Shaping Skylines.
+            </h2>
+            <p className={`${mulish.className} mt-4 lg:max-w-lg `}>
+              Roof tiles, high‑quality steel, aluminium & stainless‑steel
+              fabrication, erection and civil contracting for commercial,
+              industrial and high‑end residential projects.
+            </p>
+            <p className={`${mulish.className} mt-4 lg:max-w-lg `}>
+              From iconic clay roofing to advanced steel and aluminium structures — delivering excellence, durability and design innovation for over 25 years.
+            </p>
+            <div className="mt-11 text-right">
+              <Link
+                className="text-white py-3.5 px-6 bg-gradient-to-r from-tall-poppy-600 to-tall-poppy-700 rounded-4xl hover:rounded-lg transition-[border-radius] duration-400 ease shadow-lg cursor-pointer"
+                href="/about-us"
+              >
+                Read About Us 
+              </Link>
+            </div>
+          </div>
+        </div>
+
+      </section>
     </>
   );
 }
--- app/ui/scrollToTop.tsx
+++ app/ui/scrollToTop.tsx
@@ -24,7 +24,7 @@ const ScrollToTop = () => {
       onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
       className={`${
         scrollToTopBtn ? "visible opacity-100" : "invisible opacity-0"
-      } fixed cursor-pointer bottom-2 right-2 md:bottom-6 md:right-5 z-20 w-7 h-7 md:w-12 md:h-12 rounded-sm md:rounded-lg md:shadow-md text-[#fff] bg-gradient-to-r from-tall-poppy-700 to-tall-poppy-600 transition duration-1000 ease-in `}
+      } fixed cursor-pointer bottom-2 right-2 md:bottom-6 md:right-5 z-20 w-7 h-7 md:w-12 md:h-12 rounded-sm md:rounded-lg md:shadow-md text-[#fff] bg-gradient-to-r from-tall-poppy-600 to-tall-poppy-700 transition duration-1000 ease-in `}
     >
       <FontAwesomeIcon icon={faChevronUp} className='md:text-2xl text-sm hover:-translate-y-1 transition duration-200 ease-in-out' />
     </button>
